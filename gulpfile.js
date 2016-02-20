c3c90b1e0609b0dabd69435144a5d1aba90dcd99
En Sabah Nur <kelsey.jackson@rockfishinteractive.com>
2016-02-20 03:28:22 -0600

    InitialCommit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass");
+
+
+gulp.task("compileSass", function(){
+  gulp.src("./scss/styles.scss")
+  .pipe(sass())
+  .pipe(gulp.dest("./css/"));
+});
+
+gulp.task("watchFiles", function(){
+  gulp.watch("./scss/**/*.scss", ["compileSass"]);
+});
+
+gulp.task("serve", ["watchFiles"]);
